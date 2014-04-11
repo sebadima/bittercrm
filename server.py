b662751cc4457c62bf025acb160f452d4690f437
sebadima-digital-ocean-dev1 <me@sebadima.com>
2014-04-11 10:10:55 -0400

    testing ignore server.py

--- server.py
+++ server.py
@@ -12,7 +12,7 @@ if __name__ == '__main__':
     server = cherrypy._cpserver.Server()
 
     server.socket_host = "0.0.0.0"
-    server.socket_port = 8040
+    server.socket_port = 8042
     server.thread_pool = 30
 
 
