e4bb45cfef8e25f80fa212a3ba10917bdbad2f8b
sebadima-digital-ocean-dev1 <me@sebadima.com>
2014-04-11 10:54:11 -0400

    auto add

--- mysite/settings.py
+++ mysite/settings.py
@@ -113,7 +113,7 @@ WSGI_APPLICATION = 'mysite.wsgi.application'
 TEMPLATE_DIRS = (
     # Put strings here, like "/home/html/django_templates" or "C:/www/django/templates".
     # Always use forward slashes, even on Windows.
-    "/root/dena/pdev/pdev/mysite/templates", 
+    "/root/crm-server-002/crm-source/mysite/templates"
     # Don't forget to use absolute paths, not relative paths.
 )
 
