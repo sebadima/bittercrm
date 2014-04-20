b2ed07e4c81c8b502c33b989366eb86ffe9a8fd0
sebadima-digital-ocean-dev1 <me@sebadima.com>
2014-04-20 12:30:35 -0400

    eliminate tracce di clienti

--- mysite/urls.py
+++ mysite/urls.py
@@ -13,16 +13,9 @@ from django.contrib import admin
 admin.autodiscover()
 
 urlpatterns = patterns('',
-    # Examples:
-    # url(r'^$', 'mysite.views.home', name='home'),
-    # url(r'^mysite/', include('mysite.foo.urls')),
-    # Uncomment the admin/doc line below to enable admin documentation:
-    # url(r'^admin/doc/', include('django.contrib.admindocs.urls')),
-
-    url(r'^clienti/', include('clienti.urls')),
     url('^$', TemplateView.as_view(template_name='index.html')),
     url('^about$', TemplateView.as_view(template_name='about.html')),
-    url('^lancio$', TemplateView.as_view(template_name='lancio.html')),
+    url('^launch$', TemplateView.as_view(template_name='launch.html')),
     url(r'^admin/', include(admin.site.urls)),
 )
 
