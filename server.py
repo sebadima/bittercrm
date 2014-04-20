c36e415d91480d758a38dcadd35b191f0a1ff056
sebadima-digital-ocean-dev1 <me@sebadima.com>
2014-04-20 12:34:15 -0400

    cambiata porta

--- server.py
+++ server.py
@@ -12,7 +12,7 @@ if __name__ == '__main__':
     server = cherrypy._cpserver.Server()
 
     server.socket_host = "0.0.0.0"
-    server.socket_port = 8042
+    server.socket_port = 8044
     server.thread_pool = 30
 
 
