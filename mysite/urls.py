ccd7ec210bd8139b63e9295db8c0ce9e55db6076
sebadima-digital-ocean-dev1 <me@sebadima.com>
2014-04-21 12:02:27 -0400

    rimosso about

--- mysite/urls.py
+++ mysite/urls.py
@@ -14,7 +14,6 @@ admin.autodiscover()
 urlpatterns = patterns('',
     url(r'^contacts/', include('contacts.urls')),
     url('^$', TemplateView.as_view(template_name='index.html')),
-    url('^about$', TemplateView.as_view(template_name='about.html')),
     url('^launch$', TemplateView.as_view(template_name='launch.html')),
     url(r'^admin/', include(admin.site.urls)),
 )
