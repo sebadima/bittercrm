dcc76bb413c8f3cd908cbd993e2db9a35886326c
sebadima-digital-ocean-dev1 <me@sebadima.com>
2014-04-21 10:27:43 -0400

    aggiunto sort dei campi

--- contacts/admin.py
+++ contacts/admin.py
@@ -13,18 +13,22 @@ from contacts.models import Message
 class ContactAdmin(admin.ModelAdmin):
 	search_fields = ['nickname', 'email']
 	list_display  = ['nickname', 'email']
+        ordering      = ('nickname',)
 
 class CategoryAdmin(admin.ModelAdmin):
 	search_fields = ['code', 'desc']
 	list_display  = ['code', 'desc']
+        ordering      = ('code',)
 
 class ActionAdmin(admin.ModelAdmin):
 	search_fields = ['code', 'desc']
 	list_display  = ['code', 'desc']
+        ordering      = ('code',)
 
 class MessageAdmin(admin.ModelAdmin):
 	search_fields = ['code', 'desc']
 	list_display  = ['code', 'desc']
+        ordering      = ('code',)
 
 admin.site.register(Message,  MessageAdmin)
 admin.site.register(Contact,  ContactAdmin)
@@ -34,4 +38,3 @@ admin.site.register(Action,   ActionAdmin)
 admin.site.unregister(User)
 admin.site.unregister(Group)
 admin.site.unregister(Site)
-
