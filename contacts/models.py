34b205633409011900f5af8d3a33c3aafce30352
sebadima-digital-ocean-dev1 <me@sebadima.com>
2014-05-12 13:31:35 -0400

    fixed description

--- contacts/models.py
+++ contacts/models.py
@@ -10,10 +10,14 @@ import datetime
 class Essay(models.Model):
     name = models.CharField(max_length=2000, unique=False)
     short_desc = models.TextField(unique=False)
+    def __unicode__(self): 
+	return self.name
 
 class Para(models.Model):
     text = models.TextField(unique=False)
     essay = models.ForeignKey(Essay, unique=False)
+    def __unicode__(self): 
+	return self.text
 class Message(models.Model):
     code = models.CharField(max_length=10)
     desc = models.CharField(max_length=100)
