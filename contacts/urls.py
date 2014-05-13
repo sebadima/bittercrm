b5ff0853f425c0d2d4ef0b26c60d3616aa6de3eb
sebadima-digital-ocean-dev1 <me@sebadima.com>
2014-05-13 13:31:01 -0400

    rimosso duplicato in url

--- contacts/urls.py
+++ contacts/urls.py
@@ -5,7 +5,6 @@ from contacts import views
 
 urlpatterns = patterns('',
     url(r'^$'                              , views.index               , name='index'     ),
-    #url(r'^mailing/$'                      , views.mailing             , name='mailing'   ),
     url(r'^mailing/([0-9]{2})/([0-9]{2})/$', views.mailing             , name='mailing'   ),
     url(r'^candidates/$'                   , views.candidates          , name='candidates'),
     url(r'^prova/$'                        , views.prova               , name='prova'     ),
